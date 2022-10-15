42de57efa8417fdf3fc7fb2cf2d7cbc59e3fb2b1
parvin-aziza <azizaparvin07@gmail.com>
2022-10-15 20:03:23 +0530

    Added Style changes for mobile view and code refactor

--- getNumber.js
+++ getNumber.js
@@ -3,24 +3,23 @@ let td=document.getElementById('table').getElementsByTagName('td');
 let arrayList=[{color:"#6F98A8",number:1},{color:"#2B8EAD",number:2},{color:"#333333",number:3},{color:"#2B8EAD",number:4},{color:"#333333",number:5}
 ,{color:"#bfbfbf",number:6},{color:"#bfbfbf",number:7},{color:"#6F98A8",number:8},{color:"#333333",number:9}];
 
-//Initial Table values on page load
-const initialTable=(data)=>{
+// //Utility function to display each table data
+const displayTableData=(data)=>{
+    const isMediaQuery = window.matchMedia("(max-width: 375px)")
+
 for(let i=0;i<data?.length;i++){
     td[i].firstChild.nodeValue=data[i]?.number;
-    td[i].style.background=data[i]?.color;
+    if(isMediaQuery.matches){
+      td[i].style.borderLeft=`10px solid ${data[i]?.color}`;
+    }else{
+        td[i].style.background=data[i]?.color;
+    }
+    
 }
 }
 
-initialTable(arrayList);
+displayTableData(arrayList); //To show initial data on page load
 
-
-//Utility function to display each table data
-const displayNumberInTable=(arr)=>{
- for(let i=0;i<arr?.length;i++){
-    td[i].firstChild.nodeValue=arr[i]?.number;
-    td[i].style.background=arr[i]?.color;
- }
-}
 
 //To Shuffle Number
 const getShuffledArray=()=>{
@@ -31,11 +30,11 @@ const getShuffledArray=()=>{
 //To Shuffle Numbers onClick of Shuffle Button 
 const handleShuffle=()=>{
     const result=getShuffledArray();
-    displayNumberInTable(result);
+    displayTableData(result);
 }
 
 //To Sort Number onclick of Sort Button
 const handleSort=()=>{
     const sortedNumber=arrayList?.sort((a,b)=>{return (a.number-b.number)});
-    displayNumberInTable(sortedNumber);
+    displayTableData(sortedNumber);
 }
