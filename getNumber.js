797466d505c1ad6ed66eb7191af31b92d3ab27f5
parvin-aziza <azizaparvin07@gmail.com>
2022-10-15 20:09:53 +0530

    Code Refactoring

--- getNumber.js
+++ getNumber.js
@@ -1,11 +1,14 @@
 let td=document.getElementById('table').getElementsByTagName('td');
 
+//To reload page on page resize
+window.onresize=function(){ location.reload();}  
+
 let arrayList=[{color:"#6F98A8",number:1},{color:"#2B8EAD",number:2},{color:"#333333",number:3},{color:"#2B8EAD",number:4},{color:"#333333",number:5}
 ,{color:"#bfbfbf",number:6},{color:"#bfbfbf",number:7},{color:"#6F98A8",number:8},{color:"#333333",number:9}];
 
 // //Utility function to display each table data
 const displayTableData=(data)=>{
-    const isMediaQuery = window.matchMedia("(max-width: 375px)")
+    const isMediaQuery = window.matchMedia("(max-width: 375px)") // Media Size as per given criteria
 
 for(let i=0;i<data?.length;i++){
     td[i].firstChild.nodeValue=data[i]?.number;
